8daa4f1de8bf80579ab31c6cb0452d2229f9e05b
Darcy Liu <darcy.ldx@gmail.com>
2024-10-04 00:05:22 -0400

    removed some more garbage

---
+++ obj/zero-shot-classifier/questionansw.js
@@ -0,0 +1,4 @@
+// Python
+// from transformers import pipeline
+// oracle = pipeline(model="deepset/roberta-base-squad2")
+// oracle(question="Where do I live?", context="My name is Wolfgang and I live in Berlin")
