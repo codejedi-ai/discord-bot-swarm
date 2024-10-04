a1ba45dff1469441327c473ef7a56d4efc9b40ac
Darcy Liu <darcy.ldx@gmail.com>
2024-10-04 00:07:35 -0400

    removed node_modules

--- obj/zero-shot-classifier/questionansw.js
+++ obj/zero-shot-classifier/questionansw.js
@@ -2,3 +2,8 @@
 // from transformers import pipeline
 // oracle = pipeline(model="deepset/roberta-base-squad2")
 // oracle(question="Where do I live?", context="My name is Wolfgang and I live in Berlin")
+
+// // Use a different model for sentiment-analysis
+// let pipe = await pipeline('sentiment-analysis', 'Xenova/bert-base-multilingual-uncased-sentiment');
+
+// however it is using the pythobn code from transformers import pipeline
