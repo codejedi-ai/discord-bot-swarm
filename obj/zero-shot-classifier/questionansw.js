8c589e7b7bde08703a4f2c4766f7b4dea77c46c5
Darcy Liu <darcy.ldx@gmail.com>
2024-10-04 00:09:34 -0400

    huggingface pipelines

--- obj/zero-shot-classifier/questionansw.js
+++ obj/zero-shot-classifier/questionansw.js
@@ -7,3 +7,12 @@
 // let pipe = await pipeline('sentiment-analysis', 'Xenova/bert-base-multilingual-uncased-sentiment');
 
 // however it is using the pythobn code from transformers import pipeline
+
+const answerer = await pipeline('question-answering', 'Xenova/distilbert-base-uncased-distilled-squad');
+const question = 'Who was Jim Henson?';
+const context = 'Jim Henson was a nice puppet.';
+const output = await answerer(question, context);
+// {
+//   answer: "a nice puppet",
+//   score: 0.5768911502526741
+// }
--- obj/zero-shot-classifier/zershotclassifier.js
+++ obj/zero-shot-classifier/zershotclassifier.js
@@ -25,3 +25,12 @@
 // oracle = pipeline(model="deepset/roberta-base-squad2")
 // oracle(question="Where do I live?", context="My name is Wolfgang and I live in Berlin")
 
+const classifier = await pipeline('zero-shot-classification', 'Xenova/mobilebert-uncased-mnli');
+const text = 'Last week I upgraded my iOS version and ever since then my phone has been overheating whenever I use your app.';
+const labels = [ 'mobile', 'billing', 'website', 'account access' ];
+const output = await classifier(text, labels);
+// {
+//   sequence: 'Last week I upgraded my iOS version and ever since then my phone has been overheating whenever I use your app.',
+//   labels: [ 'mobile', 'website', 'billing', 'account access' ],
+//   scores: [ 0.5562091040482018, 0.1843621307860853, 0.13942646639336376, 0.12000229877234923 ]
+// }
